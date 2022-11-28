9c43d5e898d74139c3a2e4f144fa6c0cab02eab3
ibukunfolay <70657237+ibukunfolay@users.noreply.github.com>
2022-11-28 23:58:06 +0100

    fix: google oauth options

--- lib/utils/getGoogleUrl.ts
+++ lib/utils/getGoogleUrl.ts
@@ -5,7 +5,7 @@ const getGoogleOauthUrl = () => {
     redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL as string,
     client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
     access_type: 'offline',
-    response_type: 'offline',
+    response_type: 'code',
     prompt: 'consent',
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
